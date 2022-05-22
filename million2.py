a6ee7f6d10913838aa42be3526168852eb10f205
@DenBelinskiy <denv7781@gmail.com>
2022-05-22 14:52:18 +0300

    Миллион 3 работает

--- million2.py
+++ million2.py
@@ -1,1 +1,14 @@
-print("сколько можеш инвестировать")
+print("Какаую сумму ты хочешь инвестировать: ")
+Capital = float(input())
+print("Процентная ставка: ", end="")
+Persent = float(input())
+Term = 0
+while Capital < 1000000:
+    Free = Capital * Persent / 100
+    Capital = Capital + Free
+    Term += 1
+if Term > 0:
+    print("Чтобы превратиться в миллионера, твои деньги в течении", Term,\
+          "лет должны храниться в банкею")
+else:
+    print("Добро пожаловать в клуб милионеров")
---
+++ million3.py
@@ -0,0 +1,14 @@
+print("Какаую сумму ты хочешь инвестировать: ")
+Capital = float(input())
+print("Процентная ставка: ")
+Percent = float(input())
+print("На какой срок вкладываеш деньги: ")
+Term = int(input())
+for N in range(Term):
+    Free = Capital * Percent / 100
+    Capital = Capital + Free
+print("Ты получиш",Capital,"рублей")
+if Capital < 1000000:
+    print("Но так ты не станеш миллионером")
+else:
+    print("Добро пожаловать в клуб миллионеров")
